79db9a654bc19f9092c9195ec2364b5964c39bfd
RichardPickman <drezzerock@gmail.com>
2023-06-23 19:26:10 +0200

    add editor theme

---
+++ src/theme/index.ts
@@ -0,0 +1,25 @@
+export const theme = {
+    paragraph: 'mb-2 last:mb-0 empty:h-5 empty:mb-0',
+    quote: 'p-4 my-4 border-l-4 border-gray-300 bg-gray-50 text-base text-gray-500',
+    heading: {
+        h1: 'text-3xl',
+        h2: 'text-2xl',
+        h3: 'text-xl',
+    },
+    list: {
+        nested: {
+            listitem: 'ml-4',
+        },
+        ol: 'list-decimal ml-8',
+        ul: 'list-disc ml-4',
+        listitemChecked: 'list-none border border-black rounded bg-blue-50',
+        listitemUnchecked: 'list-none border border-black rounded bg-transparent',
+    },
+    image: 'inline-block',
+    link: 'text-blue-500',
+    linebreak: 'block w-full h-5',
+    text: {
+        underline: 'underline underline-offset-2',
+        underlineStrikethrough: 'underline underline-offset-2',
+    },
+};
