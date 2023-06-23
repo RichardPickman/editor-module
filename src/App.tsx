4ba9721085b84663a75d27879d453c091bb50b3d
RichardPickman <drezzerock@gmail.com>
2023-06-23 19:26:03 +0200

    update pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Editor from "./components/Editor";
+import { theme } from "./theme";
 
-function App() {
+import styles from './index.module.css';
+
+const App = () => {
+  const [state, setState] = useState({});
+  const onUpload = async () => 'asd';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.root}>
+      <Editor theme={theme} onUpload={onUpload} onChange={editorState => setState({ ...state, body: editorState.toJSON() })} />
     </div>
   );
 }
